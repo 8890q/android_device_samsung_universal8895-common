498a8d63081c0fe66975fb8c739d4cd96f4e6153
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-12-24 16:18:14 +0100

    universal8895-common: build fastcharge hidl
    Change-Id: Iec1a7faec24f0d5b4b1d2f1ef52b781368c5bd51

--- device-common.mk
+++ device-common.mk
@@ -103,6 +103,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_HOST_PACKAGES += \
     dtbhtoolExynos
 
+# FastCharge
+PRODUCT_PACKAGES += \
+    vendor.lineage.fastcharge@1.0-service.samsung
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
