f18edc8dc0fb9059859fe1ed894916504f21e48c
Tim Zimmermann <tim@linux4.de>
2022-02-22 15:38:03 +0100

    universal8895-common: Switch to Samsung USB 1.3 HAL
    Change-Id: I5a7a39e0b731606470b05021bb275e8caf640953

--- device-common.mk
+++ device-common.mk
@@ -313,7 +313,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.3-service.samsung
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
