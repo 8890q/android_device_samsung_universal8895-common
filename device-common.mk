348a2d47516194daadaf0596f390364d72b2e81b
ivanmeler <i_ivan@windowslive.com>
2022-03-11 14:04:51 +0000

    Import old power hal from hw/samsung

--- device-common.mk
+++ device-common.mk
@@ -188,7 +188,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service.exynos
+    android.hardware.power@1.0-service.exynos8895
 
 # Permissions
 PRODUCT_COPY_FILES += \
