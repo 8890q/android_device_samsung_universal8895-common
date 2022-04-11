be58446a68081ed9d195e8e7c1490c7d10c298ff
ivanmeler <i_ivan@windowslive.com>
2022-04-11 18:38:53 +0000

    DNM: allow neverallows

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -150,6 +150,7 @@ include device/lineage/sepolicy/exynos/sepolicy.mk
 BOARD_SEPOLICY_TEE_FLAVOR := mobicore
 include device/samsung_slsi/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/samsung/universal8895-common/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Ril
 ENABLE_VENDOR_RIL_SERVICE := true
