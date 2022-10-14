08d6f284aecf9f295ebee330bc3bb7e05dff9afa
Ivan Meler <i_ivan@windowslive.com>
2022-10-14 19:08:48 +0200

    universal8895-common: switch to aidl usb hal

--- device-common.mk
+++ device-common.mk
@@ -316,7 +316,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.samsung
+    android.hardware.usb-service.samsung
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
