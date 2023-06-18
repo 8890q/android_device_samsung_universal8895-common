d485cad8eb098c8131076be335866ddcbeec3471
roynatech2544 <whiteshell2544@naver.com>
2023-06-18 11:14:36 +0900

    universal8895: Switch to sensor impl from hardware/samsung

--- device-common.mk
+++ device-common.mk
@@ -278,8 +278,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0 \
-    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-impl.samsung \
     android.hardware.sensors@1.0-service
 
 # Setup dalvik vm configs
