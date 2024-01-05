b79bb3fdd783eb7983754f70966092013c13c9ee
Simon1511 <me@simon1511.de>
2024-01-05 17:20:59 +0100

    universal8895-common: libsec-ril: Always return an empty value for  GET_SMSC_ADDRESS
    * Make parameter of type RilData (second last one) passed to SecRil::RequestComplete()
      in OnGetSmscAddressDone() be NULL, so that the framework passes an empty value when sending SMS
    * Before U this value was not being used, on U the value returned is "SMSC",format_specifier
      which is considered invalid when passed back to RIL for sending SMS, however when an empty value
      is passed RIL will select the default SMSC, like it did on T and sending SMS works again
    
    Change-Id: I608be406969d7fa58f15ef36340aff4ad1ce9fd0

--- extract-files.sh
+++ extract-files.sh
@@ -71,6 +71,13 @@ sed -i "s/XTRA_SERVER_2/LONGTERM_PSDS_SERVER_2/" $BLOB_ROOT/etc/gps_debug.conf
 # replace SSLv3_client_method with SSLv23_method
 sed -i "s/SSLv3_client_method/SSLv23_method\x00\x00\x00\x00\x00\x00/" $BLOB_ROOT/vendor/bin/hw/gpsd
 
+# RIL patches
+xxd -p -c0 $BLOB_ROOT/proprietary/vendor/lib64/libsec-ril-dsds.so | sed "s/600e40f9820c8052e10315aae30314aa/600e40f9820c8052e10315aa030080d2/g" | xxd -r -p > $BLOB_ROOT/proprietary/vendor/lib64/libsec-ril-dsds.so.patched
+mv $BLOB_ROOT/proprietary/vendor/lib64/libsec-ril-dsds.so.patched $BLOB_ROOT/proprietary/vendor/lib64/libsec-ril-dsds.so
+
+xxd -p -c0 $BLOB_ROOT/proprietary/vendor/lib64/libsec-ril.so | sed "s/600e40f9820c8052e10315aae30314aa/600e40f9820c8052e10315aa030080d2/g" | xxd -r -p > $BLOB_ROOT/proprietary/vendor/lib64/libsec-ril.so.patched
+mv $BLOB_ROOT/proprietary/vendor/lib64/libsec-ril.so.patched $BLOB_ROOT/proprietary/vendor/lib64/libsec-ril.so
+
 # Audio hal bt sco shim
 "${PATCHELF}" --add-needed libaudioparams_shim.so $BLOB_ROOT/lib/hw/audio.primary.exynos8895.so
 sed -i 's/str_parms_get_str/str_parms_get_mod/g' $BLOB_ROOT/lib/hw/audio.primary.exynos8895.so
